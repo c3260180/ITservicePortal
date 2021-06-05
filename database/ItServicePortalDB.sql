82df0b795cfaa44ccb8b05ec01c2d503fe487eae
DawyJ <69335600+DawyJ@users.noreply.github.com>
2021-06-05 18:11:11 +1000

    db update

--- database/ItServicePortalDB.sql
+++ database/ItServicePortalDB.sql
@@ -20,29 +20,28 @@ userRole VARCHAR (80)
 CREATE TABLE ticket (
 
 userAssigned VARCHAR (80) FOREIGN KEY REFERENCES userAccount(userName),
-ticketID VARCHAR(80),
 ticketKeyword VARCHAR (80),
 ticketStatus VARCHAR (80),
 ticketTitle VARCHAR(80),
 ticketOpened VARCHAR (80),
 ticketCategory VARCHAR(80),
 ticketDescription VARCHAR(500),
+ticketComment VARCHAR (8000)
 );
 
 CREATE TABLE knowledgeBase (
 
+userAssigned VARCHAR (80) FOREIGN KEY REFERENCES userAccount(userName),
 kbTitle VARCHAR (80),
 kbCategory VARCHAR (80),
-kbTicketSubCategory VARCHAR (80),
 kbTicketOpenedTime VARCHAR (80),
 kbTicketClosedTime VARCHAR (80),
-kbUsername VARCHAR (80),
 kbDescription VARCHAR (500),
 kbKeyword VARCHAR (80),
-kbComment VARCHAR (80),
+kbComment VARCHAR (8000),
 kbResolution VARCHAR (500)
 );
 
 DROP TABLE ticket, userAccount, knowledgeBase;
 
-SELECT * FROM ticket;
+SELECT * FROM userAccount;
